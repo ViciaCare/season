d684992bd7b5d1afcd4ffd1bacd6d6dabb40361a
ViciaCare <pvk.sakha@yandex.ru>
2021-07-20 15:43:45 +0900

    reformat code

--- season/season.py
+++ season/season.py
@@ -1,16 +1,15 @@
-i=int(input("Введите месяц цифрой от 1 до 12 " ))
+i = int(input("Введите месяц цифрой от 1 до 12 "))
 
 
 def season():
-    if i<=2 or i==12:
+    if i <= 2 or i == 12:
         print("зима")
-    elif i>=3 or i<=5:
+    elif i >= 3 or i <= 5:
         print("весна")
-    elif i>5 or i<=8:
+    elif i > 5 or i <= 8:
         print("лето")
     else:
         print("осень")
 
 
 season()
-
